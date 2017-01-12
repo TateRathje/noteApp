7cbabd0dbb761def9540d4acdcbbeb13deb01a39
Tate Rathje <taterathje@gmail.com>
2017-01-12 13:01:56 -0800

    Finished basic note creation and editing.

--- app/assets/javascript/controllers/noteCreateController.js
+++ app/assets/javascript/controllers/noteCreateController.js
@@ -1,3 +1,12 @@
-angular.module('NoteApp').controller('NotesCreateController', function(){
-	
+angular.module('NoteApp').controller('NotesCreateController', function($scope, Note, $location){
+	$scope.note = new Note();
+	$scope.isSubmitting = false;
+
+	$scope.saveNote = function(note) {
+		$scope.isSubmitting = true;
+		note.$save().finally(function() {
+			$scope.isSubmitting = false;
+			$location.path('/notes');
+		});
+	}
 });
--- app/assets/javascript/controllers/noteShowController.js
+++ app/assets/javascript/controllers/noteShowController.js
@@ -1,3 +1,9 @@
-angular.module('NoteApp').controller('NotesShowController', function(Note, $scope, $routeParams){
+angular.module('NoteApp').controller('NotesShowController', function(Note, $scope, $routeParams, $location){
 	$scope.note = Note.get({id: $routeParams.id});
+
+	$scope.deleteNote = function(note) {
+		note.$remove().then(function (){
+			$location.path('/notes');
+		});
+	}
 });
