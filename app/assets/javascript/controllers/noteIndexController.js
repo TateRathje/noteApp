8e99a1ec1c4f73aa7d2664fca76a017c974d9a22
Tate Rathje <taterathje@gmail.com>
2017-02-27 16:52:12 -0800

    Finished category selector.

--- app/assets/javascript/controllers/noteIndexController.js
+++ app/assets/javascript/controllers/noteIndexController.js
@@ -1,3 +1,5 @@
-angular.module('NoteApp').controller('NotesIndexController', function(Note, $scope){
-	$scope.notes = Note.query();
+angular.module('NoteApp').controller('NotesIndexController', function(Note, $scope) {
+  $scope.notes = Note.query();
+  $scope.search = {};
+  window.sc = $scope;
 });
--- app/assets/javascript/controllers/usersIndexController.js
+++ app/assets/javascript/controllers/usersIndexController.js
@@ -1,4 +1,3 @@
 angular.module('NoteApp').controller('UsersIndexController', function(User, $scope){
 	$scope.users = User.query();
-	$scope.search = {};
 });
